8853628b7e34ad19bc40bab0678563f14b1d3ab6
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-09-11 22:38:59 +0200

    Add files via upload

---
+++ Interface/src/Introducible.java
@@ -0,0 +1,5 @@
+public interface Introducible
+{
+    void introduce(); // public by default
+    String INTRODUCTION = "My name is "; //public, final, static by default
+}
---
+++ Interface/src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Introducible i1 = new Person("Bartek");
+        i1.introduce();
+    }
+}
